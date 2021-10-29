5063c1158b2bb42f4fb65cec8e32733d101f6fbd
austen.shelton <austen.shelton@gmail.com>
2021-10-29 08:27:46 -0400

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import Shipping from "./pages/Shipping"
+import Billing from "./pages/Billing"
+import Home from "./pages/Home"
+import Menu from "./components/Menu"
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [customer, setCustomer] = useState({})
+    const [validated, setValidated] = useState(false)
+
+    return (
+        <Router>
+            <Menu />
+            <Switch>
+                <Route path="/shipping">
+                    <Shipping
+                        customer={customer}
+                        setCustomer={setCustomer}
+                        validated={validated}
+                        setValidated={setValidated}
+                    />
+                </Route>
+                <Route path="/billing">
+                    <Billing
+                        customer={customer}
+                        setCustomer={setCustomer}
+                        validated={validated}
+                        setValidated={setValidated}
+                    />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
