5ef11301b5ddfa662543dd04217f318d4a12b513
kadowning1 <kadowning1@gmail.com>
2021-10-29 08:29:45 -0400

    add app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Dashboard from './Pages/Dashboard'
+import NewUser from './Pages/NewUser'
+import Login from './Pages/Login'
+import NewNavBar from './Components/NewNavBar'
+// import ReactForm from './Components/ReactForm'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import Home from './Pages/Home';
+// import { Navbar } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        let lsToken = window.localStorage.getItem('token')
+        if (lsToken) {
+            setToken(lsToken)
+        }
+    }, [])
+
+    // if (!token) {
+    //     return <Redirect to="/home" />
+    // }
+
+    const saveToken = userToken => {
+        localStorage.setItem('token', userToken);
+        setToken(userToken);
+    };
+
+    const removeToken = () => {
+        localStorage.removeItem("token")
+        setToken('')
+    };
+
+
+
+    return (
+        <div className="text-center">
+            <h1>Cat Steve's Tackle Shoppe</h1>
+            <BrowserRouter>
+                <NewNavBar
+                    removeToken={removeToken}
+                    token={token} />
+                <Switch>
+                    <Route path="/dashboard">
+                        {token.length === 0 ? <Redirect to='/login' /> : <Dashboard token={token} />}
+                    </Route>
+                    <Route path="/newuser">
+                        <NewUser saveToken={saveToken} />
+                    </Route>
+                    <Route path="/login">
+                        {token.length > 0 ? <Redirect to='/dashboard' /> : <Login saveToken={saveToken} />}
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+                {/* <ReactForm />    */}
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
